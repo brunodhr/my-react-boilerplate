af94bce3496ce5a15e11b338f68a2b380f9043f9
Bruno Filipe <brunofiliperamos@hotmail.com>
2020-02-10 17:12:18 -0300

    chore: add style themes

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'dark',
+  colors: {
+    primary: '#333',
+    secundary: '#8332AC',
+    background: '#222',
+    text: '#fff'
+  }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'light',
+  colors: {
+    primary: '#330C2F',
+    secundary: '#8332AC',
+    background: '#f5f5f5',
+    text: '#333'
+  }
+}
